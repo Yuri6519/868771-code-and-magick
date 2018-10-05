e291eaba42d8ea27cbfb140dcfb2ea2cb69f49e5
Yuri Kuzmenko <y.kuzmenko@contact-sys.com>
2018-10-05 13:51:05 +0300

    аватар юзера

---
+++ js/avatar.js
@@ -0,0 +1,33 @@
+'use strict';
+
+(function () {
+
+  var AVATAR_FILE_EXT = ['gif', 'jpg', 'jpeg', 'png'];
+  var avatarUploadFile = document.querySelector('.upload input[type=file]');
+  var avatarImg = document.querySelector('.setup-user-pic');
+
+  avatarUploadFile.addEventListener('change', function () {
+
+    var avatarFile = avatarUploadFile.files[0];
+
+    if (avatarFile) {
+      var avatarFileName = avatarFile.name.toLowerCase();
+
+      var ismatch = AVATAR_FILE_EXT.some(function (itr) {
+        return avatarFileName.endsWith(itr);
+      });
+
+      if (ismatch) {
+        var fileReader = new FileReader();
+
+        fileReader.addEventListener('load', function () {
+          avatarImg.src = fileReader.result;
+        });
+
+        fileReader.readAsDataURL(avatarFile);
+
+      }
+    }
+  });
+
+})();
